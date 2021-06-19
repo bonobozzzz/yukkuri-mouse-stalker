e6da63ff0fdc84226bf79a9668cc881c4d2d1d10
bonobozzzz <kawaiiyumegirl@gmail.com>
2021-06-19 12:01:11 +0900

    黒い点をマウスに追従するようにする

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-window.alert('hello world');
+  const IMG_WID = 48;
+  const IMG_HEI = 48;
+  const DIV_ID = 'stalker-obj';
+
+  var body_elements = document.getElementsByTagName('body');
+  var elem = document.createElement('div');
+  elem.id = 'mouse-stalker';
+  body_elements[0].appendChild(elem);
+
+
+  console.log(body_elements[0])
+  console.log(elem)
+
+
+  const stalker = document.getElementById('mouse-stalker');
+  document.addEventListener('mousemove', function (e) {
+  stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+});
